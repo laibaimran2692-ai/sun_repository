eba6aacd0f33f1a566c4ea367ffa2faded8e6c64
laibaimran2692-ai <laibaimran2692@gmail.com>
2026-02-01 17:34:07 +0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("My First Streamlit App ")
+st.write("Hello! This app is running in VS Code.")
+name = st.text_input("Enter your name:")
+if name:
+    st.success(f"Welcome, {name} ")
